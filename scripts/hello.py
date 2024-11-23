8b1724b8dd4b592b07dd65908474968e0cc6fe9c
Henry Zwart <henry.zwart@dragonfly.co.nz>
2024-11-23 18:10:45 +0100

    update ruff, fix formatting

--- scripts/hello.py
+++ scripts/hello.py
@@ -3,11 +3,11 @@ import simpy
 
 def car(env):
     while True:
-        print("Start parking at %d" % env.now)
+        print(f"Start parking at {env.now}")
         parking_duration = 5
         yield env.timeout(parking_duration)
 
-        print("Start driving at %d" % env.now)
+        print(f"Start driving at {env.now}")
         trip_duration = 2
         yield env.timeout(trip_duration)
 
