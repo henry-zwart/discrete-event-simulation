437b45ef72e11e4fd50d88370662d266d1150f33
TikaBennekum <107108939+TikaBennekum@users.noreply.github.com>
2024-11-25 16:13:44 +0100

    avg waiting time

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ import simpy
 import numpy as np
 
 SIM_TIME = 20
+AVG_WAITING_TIME = 0
+CLIENT_COUNT = itertools.count()
 
 class System:
     # setup system
@@ -27,30 +29,37 @@ class System:
         yield self.env.timeout(servicetime)
 
 def client(env, system, servicetime, id):
+    global AVG_WAITING_TIME
     # tells enviroment new customer arrives
-    print(f'a client {id} arrives the system at {env.now:.2f}.')
+    time_entering_system = env.now
+    print(f'a client {id} arrives the system at {time_entering_system:.2f}.')
     with system.server.request() as request:
         yield request
-
-        print(f'a client {id} is being serviced at {env.now:.2f}.')
+        
+        time_being_serviced = env.now
+        print(f'a client {id} is being serviced at {time_being_serviced:.2f}.')
+        AVG_WAITING_TIME += time_being_serviced - time_entering_system
         yield env.process(system.serv(servicetime))
         print(f'a client {id} stopped being serviced at {env.now:.2f}.')
 
 def setup(env, nr_servers, arrival_rate, rho):
+    global CLIENT_COUNT
     # arrival rate = lambda
     capacity = arrival_rate / (rho * nr_servers)
     system = System(env, nr_servers)
-    client_count = itertools.count()
     
     while True:
         servicetime = np.random.exponential(1 / capacity)
         yield  env.timeout(np.random.exponential(1 / arrival_rate))
-        env.process(client(env, system, servicetime, next(client_count)))
+        env.process(client(env, system, servicetime, next(CLIENT_COUNT)))
 
 
 random.seed(145)
-
 env = simpy.Environment()
+
+nr_servers = 1
 env.process(setup(env, nr_servers=1, arrival_rate=1, rho=0.95))
 env.run(until=SIM_TIME)
+AVG_WAITING_TIME /= next(CLIENT_COUNT) - 1
+print(AVG_WAITING_TIME)
 
