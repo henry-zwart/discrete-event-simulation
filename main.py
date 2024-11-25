b5daaa8ed5754539903b3f06c805fecd351a86af
TikaBennekum <107108939+TikaBennekum@users.noreply.github.com>
2024-11-25 17:26:39 +0100

    welch

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ import random
 
 import numpy as np
 import simpy
+from scipy.stats import ttest_ind
 
 SIM_TIME = 0
 AVG_WAITING_TIME = 0
@@ -23,9 +24,12 @@ CLIENT_COUNT = itertools.count()
 
 class System:
     # setup system
-    def __init__(self, env, nr_servers):
+    def __init__(self, env, nr_servers, fifo):
         self.env = env
-        self.server = simpy.Resource(env, nr_servers)
+        if fifo:
+            self.server = simpy.Resource(env, nr_servers)
+        else:
+            self.server = simpy.PriorityResource(env, nr_servers)
 
     # serving of client
     def serv(self, servicetime):
@@ -34,24 +38,27 @@ class System:
 
 def client(env, system, servicetime, id):
     global AVG_WAITING_TIME
-    # tells enviroment new customer arrives
+
     time_entering_system = env.now
     # print(f'a client {id} arrives the system at {time_entering_system:.2f}.')
+
+    # tells enviroment new customer arrives
     with system.server.request() as request:
         yield request
 
         time_being_serviced = env.now
         # print(f'a client {id} is being serviced at {time_being_serviced:.2f}.')
         AVG_WAITING_TIME += time_being_serviced - time_entering_system
+
         yield env.process(system.serv(servicetime))
         # print(f'a client {id} stopped being serviced at {env.now:.2f}.')
 
 
-def setup(env, nr_servers, arrival_rate, rho):
+def setup(env, nr_servers, capacity, rho, fifo):
     global CLIENT_COUNT
     # arrival rate = lambda
-    capacity = arrival_rate / (rho * nr_servers)
-    system = System(env, nr_servers)
+    arrival_rate = rho * nr_servers * capacity
+    system = System(env, nr_servers, fifo)
 
     while True:
         servicetime = np.random.exponential(1 / capacity)
@@ -59,7 +66,7 @@ def setup(env, nr_servers, arrival_rate, rho):
         env.process(client(env, system, servicetime, next(CLIENT_COUNT)))
 
 
-def run(seed, sim_time, nr_servers, arrival_rate, rho):
+def run(seed, sim_time, nr_servers, capacity, rho, fifo):
     global AVG_WAITING_TIME
     global CLIENT_COUNT
     global SIM_TIME
@@ -70,7 +77,7 @@ def run(seed, sim_time, nr_servers, arrival_rate, rho):
     random.seed(seed)
     env = simpy.Environment()
 
-    env.process(setup(env, nr_servers, arrival_rate, rho))
+    env.process(setup(env, nr_servers, capacity, rho, fifo))
     env.run(until=SIM_TIME)
     nr_clients = next(CLIENT_COUNT)
     AVG_WAITING_TIME /= nr_clients - 1
@@ -81,19 +88,28 @@ def run(seed, sim_time, nr_servers, arrival_rate, rho):
 def experiment():
     avg_wait_nr_servers = []
 
-    num_runs = 5
+    num_runs = 20
     for nr_servers in [1, 2, 4]:
         print("nr_servers: ", nr_servers)
-        waiting_time_runs = 0
+        waiting_time_runs = []
         for i in range(num_runs):
             print("run: ", i)
             nr_clients, waiting_time = run(
-                145, 10000, nr_servers, arrival_rate=1, rho=0.75
+                145, 10000, nr_servers, capacity=1, rho=0.9, fifo=False
             )
-            waiting_time_runs += waiting_time
-        avg_wait_nr_servers.append(waiting_time_runs / num_runs)
+            waiting_time_runs.append(waiting_time)
+        avg_wait_nr_servers.append(waiting_time_runs)
 
-    print(avg_wait_nr_servers)
+    avg_wait_nr_servers = np.array(avg_wait_nr_servers)
+    list_means = np.mean(avg_wait_nr_servers, axis=1)
+    list_std = np.std(avg_wait_nr_servers, axis=1, ddof=1)
+
+    print(list_means)
+    print(list_std)
+
+    # WELCH
+    welch = ttest_ind(avg_wait_nr_servers[0], avg_wait_nr_servers[1], equal_var=False)
+    print(welch)
 
 
 experiment()
