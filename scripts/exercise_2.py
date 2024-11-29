18ee1f7c9b62870729b152edef99ed86fc66111a
Henry Zwart <henry.zwart@dragonfly.co.nz>
2024-11-29 16:37:55 +0100

    absolute paths, add n_server param to main

--- scripts/exercise_2.py
+++ scripts/exercise_2.py
@@ -29,10 +29,10 @@ avg_wait_nr_servers = experiment(
 welch_1_2 = ttest_ind(avg_wait_nr_servers[0], avg_wait_nr_servers[1], equal_var=False)
 welch_1_4 = ttest_ind(avg_wait_nr_servers[0], avg_wait_nr_servers[2], equal_var=False)
 
-np.save("../data/exercise_2_avg_wait_nr_servers", avg_wait_nr_servers)
+np.save("data/exercise_2_avg_wait_nr_servers", avg_wait_nr_servers)
 
 
-with open("../results/exercise_2.txt", "w") as f:
+with open("results/exercise_2.txt", "w") as f:
     f.write(f"{welch_1_2} \n")
     f.write(f"{welch_1_4} \n")
     f.close()
@@ -45,8 +45,8 @@ std_dict = {}
 rho_list = [0.5, 0.65, 0.8, 0.95]
 # num_runs_list = [x for x in range(5,5,200)]
 sim_time_list = [x for x in range(100, 300, 50)]
-np.save("../data/sim_time_list", sim_time_list)
-np.save("../data/rho_list", rho_list)
+np.save("data/sim_time_list", sim_time_list)
+np.save("data/rho_list", rho_list)
 for rho in rho_list:
     means_dict[(rho)] = []
     std_dict[(rho)] = []
@@ -69,7 +69,7 @@ lower = {}
 for rho in rho_list:
     lower[(rho)] = np.array(means_dict[(rho)]) - np.array(std_dict[(rho)]) * 2
     upper[(rho)] = np.array(means_dict[(rho)]) + np.array(std_dict[(rho)]) * 2
-    np.save(f"../data/means_{rho}", means_dict[(rho)])
-    np.save(f"../data/std_{rho}", std_dict[(rho)])
-    np.save(f"../data/lower_{rho}", lower[(rho)])
-    np.save(f"../data/upper_{rho}", upper[(rho)])
+    np.save(f"data/means_{rho}", means_dict[(rho)])
+    np.save(f"data/std_{rho}", std_dict[(rho)])
+    np.save(f"data/lower_{rho}", lower[(rho)])
+    np.save(f"data/upper_{rho}", upper[(rho)])
